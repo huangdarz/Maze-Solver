aa79325bc5dc26cee0e4e8cbbc1d72e17b2847d9
Darin <huangdarin465@gmail.com>
2020-04-30 04:36:27 +1000

    Added main method

--- maze_solver.ui/src/maze_solver/ui/Main.java
+++ maze_solver.ui/src/maze_solver/ui/Main.java
@@ -6,6 +6,11 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setScene(new Scene(new Pane(), 300, 275));
